2b45b99884c3b02962466dbb45aad15ed9254aab
Jasson Cu <85533418+JassonCu@users.noreply.github.com>
2023-06-22 03:46:10 +0000

    Add two news work filters

--- fronted_react/src/container/Work/Work.jsx
+++ fronted_react/src/container/Work/Work.jsx
@@ -41,7 +41,7 @@ const Work = () => {
       <h2 className="head-text">My Creative <span>Portfolio</span> Section</h2>
 
       <div className="app__work-filter">
-        {['UI/UX', 'React JS', 'All'].map((item, index) => (
+        {['UI/UX', 'React JS', 'Vue', 'Go', 'All'].map((item, index) => (
           <div
             key={index}
             onClick={() => handleWorkFilter(item)}
